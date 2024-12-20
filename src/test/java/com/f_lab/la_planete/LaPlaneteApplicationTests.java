241358983cbd3f2ed90bb24c002a90348101dbf4
koreanMike513 <paulryu1998@gmail.com>
2024-12-20 14:04:54 +0000

    init: Added spring skeleton codes
    closes #5

---
+++ src/test/java/com/f_lab/la_planete/LaPlaneteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f_lab.la_planete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaPlaneteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
