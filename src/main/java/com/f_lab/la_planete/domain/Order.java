e817c452a8b4d918386e021c59e2302414342bcc
koreanMike513 <paulryu1998@gmail.com>
2024-12-23 22:58:40 +0000

    update: Updated basic domains
    - added more fields and methods to domain classes
    - fixed import statements (from * to individual import)

--- src/main/java/com/f_lab/la_planete/domain/Order.java
+++ src/main/java/com/f_lab/la_planete/domain/Order.java
@@ -1,9 +1,23 @@
 package com.f_lab.la_planete.domain;
 
 import com.f_lab.la_planete.domain.base.BaseEntity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -11,6 +25,8 @@ import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "orders")
 public class Order extends BaseEntity {
@@ -19,10 +35,28 @@ public class Order extends BaseEntity {
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
 
+  @ManyToOne
+  @JoinColumn(name = "food_id")
+  private Food food;
+
+  private BigDecimal totalCost;
+
+  private int quantity;
+
+  // 화폐 추가
+
   @Enumerated(EnumType.STRING)
   private OrderStatus status;
 
   @OneToOne(fetch = LAZY)
   @JoinColumn(name = "payment_id")
   private Payment payment;
+
+  public void updateTotalCost(BigDecimal totalCost) {
+    this.totalCost = totalCost;
+  }
+
+  public BigDecimal calculateTotalCost() {
+    return food.calculateCost(quantity);
+  }
 }
--- src/main/java/com/f_lab/la_planete/domain/Payment.java
+++ src/main/java/com/f_lab/la_planete/domain/Payment.java
@@ -1,18 +1,26 @@
 package com.f_lab.la_planete.domain;
 
 import com.f_lab.la_planete.domain.base.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
-@Getter
+@Getter @Setter(PRIVATE)
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "payments")
 public class Payment extends BaseEntity {
@@ -20,11 +28,19 @@ public class Payment extends BaseEntity {
   @Id @GeneratedValue(strategy = IDENTITY)
   private Long id;
 
-  private BigDecimal amount;
+  private BigDecimal totalCost;
 
   @Enumerated(EnumType.STRING)
   private PaymentStatus status;
 
   @OneToOne(mappedBy = "payment", fetch = LAZY)
   private Order order;
+
+  public static Payment of(BigDecimal totalCost, Order order) {
+    Payment payment = new Payment();
+    payment.setTotalCost(totalCost);
+    payment.setOrder(order);
+    payment.setStatus(PaymentStatus.READY);
+    return payment;
+  }
 }
