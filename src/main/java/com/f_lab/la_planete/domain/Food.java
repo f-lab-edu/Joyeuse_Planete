63dfa5abdd62a3c350a75c09b8cd7b1b3e71a45a
koreanMike513 <paulryu1998@gmail.com>
2024-12-23 22:58:40 +0000

    update: Updated basic domains
    - added more fields and methods to domain classes
    - fixed import statements (from * to individual import)

--- src/main/java/com/f_lab/la_planete/domain/Food.java
+++ src/main/java/com/f_lab/la_planete/domain/Food.java
@@ -1,7 +1,14 @@
 package com.f_lab.la_planete.domain;
 
 import com.f_lab.la_planete.domain.base.BaseEntity;
-import jakarta.persistence.*;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,23 +33,27 @@ public class Food extends BaseEntity {
 
   private BigDecimal price;
 
-  private int quantity;
+  private int totalQuantity;
 
   @ManyToOne(fetch = LAZY)
   @JoinColumn(name = "store_id")
   private Store store;
 
-  public static Food of(BigDecimal price, int quantity) {
+  public static Food of(BigDecimal price, int totalQuantity) {
     Food food = new Food();
     food.setPrice(price);
-    food.setQuantity(quantity);
+    food.setTotalQuantity(totalQuantity);
     return food;
   }
 
+  public BigDecimal calculateCost(int quantity) {
+    return BigDecimal.valueOf((long) price.intValue() * quantity);
+  }
+
   public void minusQuantity(int deductions) {
-    if (quantity - deductions < 0)
+    if (totalQuantity - deductions < 0)
       throw new IllegalStateException("수량이 부족합니다. 따라서 구매가 진행될 수 없습니다.");
 
-    quantity -= deductions;
+    totalQuantity -= deductions;
   }
 }
