0164d652b1a4b37c1ec93eca66821fb8337c4462
koreanMike513 <paulryu1998@gmail.com>
2024-12-29 22:06:04 +0000

    update: Updated OrderCreateRequestDTO

--- src/main/java/com/f_lab/la_planete/dto/request/OrderCreateRequestDTO.java
+++ src/main/java/com/f_lab/la_planete/dto/request/OrderCreateRequestDTO.java
@@ -4,16 +4,24 @@ import com.f_lab.la_planete.domain.Food;
 import com.f_lab.la_planete.domain.Order;
 import com.f_lab.la_planete.domain.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderCreateRequestDTO {
 
   @JsonProperty("food_id")
   private Long foodId;
+
   private BigDecimal price;
+
   private int quantity;
 
   public Order toEntity(Food food) {
