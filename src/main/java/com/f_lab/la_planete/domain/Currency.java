4b04ef0046255b47e62934c75415e3676a8cc02b
koreanMike513 <paulryu1998@gmail.com>
2024-12-29 21:41:59 +0000

    feat: Added Currency entity

---
+++ src/main/java/com/f_lab/la_planete/domain/Currency.java
@@ -0,0 +1,43 @@
+package com.f_lab.la_planete.domain;
+
+import com.f_lab.la_planete.domain.base.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.RoundingMode;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "currency")
+public class Currency extends BaseTimeEntity {
+
+  @Id @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String currencyCode;
+
+  @Column(nullable = false)
+  private String currencySymbol;
+
+  @Column(nullable = false)
+  private int roundingScale;
+
+  @Enumerated(EnumType.STRING)
+  @Column(columnDefinition = "VARCHAR(20) NOT NULL DEFAULT 'FLOOR'")
+  private RoundingMode roundingMode;
+}
