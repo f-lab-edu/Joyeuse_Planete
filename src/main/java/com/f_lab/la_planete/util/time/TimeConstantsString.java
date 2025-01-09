07add95cbbbe0dd1783e906a021faaeb640126d0
koreanMike513 <paulryu1998@gmail.com>
2025-01-09 18:38:14 +0000

    feat: Added Spring AOP
    - added LockRetryAspect that runs retry logic on lock failures

--- src/main/java/com/f_lab/la_planete/util/time/TimeConstantsString.java
+++ src/main/java/com/f_lab/la_planete/util/time/TimeConstantsString.java
@@ -2,4 +2,6 @@ package com.f_lab.la_planete.util.time;
 
 public class TimeConstantsString {
   public static final String FIVE_SECONDS = "5000";
+
+  public static final String ONE_SECOND = "1000";
 }
