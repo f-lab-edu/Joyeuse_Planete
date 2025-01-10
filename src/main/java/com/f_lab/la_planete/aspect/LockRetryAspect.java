22e591839314b09a42dcb847f5ebc5349a620b21
koreanMike513 <paulryu1998@gmail.com>
2025-01-10 19:14:58 +0000

    fix: Fixed Lock Retry AOP with exponential backoff and custom lock retries

--- src/main/java/com/f_lab/la_planete/aspect/LockRetryAspect.java
+++ src/main/java/com/f_lab/la_planete/aspect/LockRetryAspect.java
@@ -7,30 +7,41 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
 @Component
 public class LockRetryAspect {
-  private static final int MAX_RETRY = 3;
-  private static final int STOP_TIME = Integer.parseInt(TimeConstantsString.ONE_SECOND);
+
+  @Value("${lock.max.retry:3}")
+  private int MAX_RETRY;
+  private static final int FIRST_WAIT_INTERVAL = Integer.parseInt(TimeConstantsString.ONE_SECOND);
+  private static final int MULTIPLIER = 2;
 
   @Around("@annotation(com.f_lab.la_planete.aspect.RetryOnLockFailure)")
   public Object lockRetry(ProceedingJoinPoint joinPoint) {
-    int attempts = 0;
+    int attempts = 0, stopInterval = FIRST_WAIT_INTERVAL;
+
+    log.info("ARGS={}", joinPoint.getArgs());
 
     while (attempts < MAX_RETRY) {
       try {
         return joinPoint.proceed();
       } catch (PessimisticLockException | LockTimeoutException e) {
         attempts++;
-        log.warn("시도 횟수={}, 다시 메서드={} 파라미터={} 락을 얻기를 시도합니다",
-            attempts, joinPoint.getSignature(),joinPoint.getArgs());
+        log.warn("시도 횟수={}, 다시 메서드={} 락을 얻기를 시도합니다",
+            attempts, joinPoint.getSignature());
 
         // 재시도 전 잠시 멈추고 다시 시작
+        // 각 시도 마다 WAIT_INTERVAL 이 MULTIPLIER 에 상응하는 값을 지수적으로 늘어납니다 (backoff)
+        // synchronised retry 를 피하기 위해 random 값을 기존 WAIT_INTERVAL 에서 10% ~ 30% 상응하는 값을 더합니다.
         try {
-          Thread.sleep(STOP_TIME);
+          double PERCENTAGE = 0.1 + (Math.random() * 0.2);
+          int RANDOM_FACTOR = (int) (stopInterval * PERCENTAGE);
+          stopInterval = stopInterval * (int) Math.pow(MULTIPLIER, attempts) - RANDOM_FACTOR;
+          Thread.sleep(stopInterval);
 
         } catch (InterruptedException ex) {
           throw new RuntimeException(ex);
