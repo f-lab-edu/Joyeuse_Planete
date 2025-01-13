c37c0ca22ec788d4cdf089c2381c6953be12a6d4
koreanMike513 <paulryu1998@gmail.com>
2025-01-13 20:58:14 +0000

    chore: Added notification project for MSA
    - added notification project
    - updated build.gradle and settings.gradle

---
+++ notifications/src/main/java/com/f_lab/la_planete/notifications/NotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.f_lab.la_planete.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsApplication.class, args);
+	}
+
+}
