aba39a7e85c31d935c0b11a1419cf6b84ae0f4c3
koreanMike513 <paulryu1998@gmail.com>
2025-01-17 21:04:46 +0000

    feat: Added Payment Kafka Command handler
    - added payment kafka command handler
    - added payment service

---
+++ payment/src/main/java/com/f_lab/la_planete/payment/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.f_lab.la_planete.payment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+}
