563e737ca7ac587698da5222eccc0fc3fe53bb84
koreanMike513 <paulryu1998@gmail.com>
2025-01-17 20:45:16 +0000

    feat: Added Kafka config in Foods project
    - added kafka consumer and producer configs
    - added FoodReserveCommandHandler
    - removed application.properties and added application.yml

---
+++ foods/src/main/java/com/f_lab/la_planete/foods/config/KafkaConsumerConfig.java
@@ -0,0 +1,61 @@
+package com.f_lab.la_planete.foods.config;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+  @Value("${spring.kafka.consumer.bootstrap-servers}")
+  private String BOOTSTRAP_SERVERS;
+
+  @Value("${spring.kafka.consumer.enable-auto-commit}")
+  private boolean AUTO_COMMIT;
+
+  @Value("${kafka.container.concurrency:3}")
+  private int CONCURRENCY;
+
+  @Value("${spring.kafka.consumer.properties.spring.json.trusted.packages")
+  private String TRUSTED_PACKAGES;
+
+  @Bean
+  public Map<String, Object> consumerConfig() {
+    Map<String, Object> consumerConfig = new HashMap<>();
+
+    consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+    consumerConfig.put(JsonDeserializer.TRUSTED_PACKAGES, TRUSTED_PACKAGES);
+    consumerConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, AUTO_COMMIT);
+
+    return consumerConfig;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+
+    factory.setConsumerFactory(consumerFactory());
+    factory.setConcurrency(CONCURRENCY);
+
+    return factory;
+  }
+
+  @Bean
+  public ConsumerFactory<String, Object> consumerFactory() {
+    return new DefaultKafkaConsumerFactory<>(consumerConfig());
+  }
+}
