48758a69b96ae0f97bd136dd54ab0487842ddf5e
koreanMike513 <paulryu1998@gmail.com>
2025-01-17 20:47:35 +0000

    fix: Added food service for successful build

--- foods/src/main/java/com/f_lab/la_planete/foods/service/FoodService.java
+++ foods/src/main/java/com/f_lab/la_planete/foods/service/FoodService.java
@@ -1,4 +1,28 @@
 package com.f_lab.la_planete.foods.service;
 
+import com.f_lab.la_planete.core.domain.Food;
+import com.f_lab.la_planete.foods.repository.FoodRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FoodService {
+
+  private final FoodRepository foodRepository;
+
+
+  @Transactional
+  public void reserve(Long foodId, int quantity) {
+    foodRepository.save(Food.builder().totalQuantity(100).price(BigDecimal.valueOf(1000)).build());
+    Food food = foodRepository.findFoodByFoodIdWithPessimisticLock(foodId);
+    food.minusQuantity(quantity);
+    foodRepository.save(food);
+  }
 }
