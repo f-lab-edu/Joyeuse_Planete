29307d94bcee5dd2f877f7c05f74e8693a7fcaa6
koreanMike513 <paulryu1998@gmail.com>
2025-01-23 19:19:53 +0000

    refactor: Refactored food kafka handler service and exceptions

--- foods/src/main/java/com/f_lab/la_planete/foods/service/FoodService.java
+++ foods/src/main/java/com/f_lab/la_planete/foods/service/FoodService.java
@@ -1,8 +1,8 @@
 package com.f_lab.la_planete.foods.service;
 
 import com.f_lab.la_planete.core.domain.Food;
-import com.f_lab.la_planete.foods.exceptions.ErrorCode;
-import com.f_lab.la_planete.foods.exceptions.FoodApplicationKafkaNonRetryableException;
+import com.f_lab.la_planete.core.exceptions.ErrorCode;
+import com.f_lab.la_planete.core.exceptions.ApplicationException;
 import com.f_lab.la_planete.foods.repository.FoodRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,13 +22,12 @@ public class FoodService {
   @Transactional
   public void reserve(Long foodId, int quantity) {
     Food food = findFoodWithLock(foodId);
-    log.info("음식 id = {}, 수량 = {}", food.getId(), food.getTotalQuantity());
     food.minusQuantity(quantity);
     foodRepository.save(food);
   }
 
   private Food findFoodWithLock(Long foodId) {
     return foodRepository.findFoodByFoodIdWithPessimisticLock(foodId)
-        .orElseThrow(() -> new FoodApplicationKafkaNonRetryableException(ErrorCode.FOOD_NOT_EXIST_EXCEPTION));
+        .orElseThrow(() -> new ApplicationException(ErrorCode.FOOD_NOT_EXIST_EXCEPTION));
   }
 }
---
+++ foods/src/main/java/com/f_lab/la_planete/foods/service/KafkaHandlerService.java
@@ -0,0 +1,37 @@
+package com.f_lab.la_planete.foods.service;
+
+import com.f_lab.la_planete.core.kafka.aspect.KafkaRetry;
+import com.f_lab.la_planete.core.kafka.exceptions.RetryableException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaHandlerService {
+
+  private final KafkaTemplate<String, Object> kafkaTemplate;
+
+  @KafkaRetry
+  @Transactional(propagation = REQUIRES_NEW)
+  public void sendKafkaEvent(String event, Object object) {
+    try {
+      kafkaTemplate.send(event, object);
+    } catch(Exception e) {
+      log.error("오류가 발생하였습니다. message = {}", e.getMessage(), e);
+      throw new RetryableException();
+    }
+  }
+
+  /**
+   * DeadLetterQueue 에 대한 구현이 안되어 있어서 아직은 technical debt로 남겨둠
+   */
+  public void sendKafkaFailureEvent(String event, Object object) {
+    kafkaTemplate.send(event, object);
+  }
+}
