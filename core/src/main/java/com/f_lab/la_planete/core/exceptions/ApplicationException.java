63cac412219c046edddf729cb16f587582204ace
koreanMike513 <paulryu1998@gmail.com>
2025-01-23 19:00:24 +0000

    refactor: Refactored exceptions in core module

---
+++ core/src/main/java/com/f_lab/la_planete/core/exceptions/ApplicationException.java
@@ -0,0 +1,23 @@
+package com.f_lab.la_planete.core.exceptions;
+
+public class ApplicationException extends RuntimeException {
+
+  public ApplicationException() {
+  }
+
+  public ApplicationException(ErrorCode errorCode) {
+    super(errorCode.getDescription());
+  }
+
+  public ApplicationException(ErrorCode errorCode, Throwable cause) {
+    super(errorCode.getDescription(), cause);
+  }
+
+  public ApplicationException(Throwable cause) {
+    super(cause);
+  }
+
+  public ApplicationException(ErrorCode errorCode, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(errorCode.getDescription(), cause, enableSuppression, writableStackTrace);
+  }
+}
