da2e82c3e15a13aaa357b94b709de5a1be9e0571
koreanMike513 <paulryu1998@gmail.com>
2025-01-23 19:19:53 +0000

    refactor: Refactored food kafka handler service and exceptions

--- foods/src/main/java/com/f_lab/la_planete/foods/service/handler/OrderCreatedEventHandler.java
+++ foods/src/main/java/com/f_lab/la_planete/foods/service/handler/OrderCreatedEventHandler.java
@@ -3,21 +3,17 @@ package com.f_lab.la_planete.foods.service.handler;
 
 import com.f_lab.la_planete.core.events.OrderCreatedEvent;
 import com.f_lab.la_planete.core.events.OrderCreationFailedEvent;
-import com.f_lab.la_planete.core.kafka.aspect.KafkaRetry;
-import com.f_lab.la_planete.core.kafka.exceptions.NonRetryableException;
-import com.f_lab.la_planete.foods.exceptions.FoodApplicationKafkaRetryableException;
 import com.f_lab.la_planete.foods.service.FoodService;
+import com.f_lab.la_planete.foods.service.KafkaHandlerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
 @Slf4j
 @Component
@@ -29,8 +25,8 @@ import static org.springframework.transaction.annotation.Propagation.REQUIRES_NE
 public class OrderCreatedEventHandler {
 
 
-  private final KafkaTemplate<String, Object> kafkaTemplate;
   private final FoodService foodService;
+  private final KafkaHandlerService kafkaHandlerService;
 
   @Value("${foods.events.topic.name}")
   private String foodReservationEvent;
@@ -40,43 +36,33 @@ public class OrderCreatedEventHandler {
 
   @KafkaHandler
   @Transactional(value = "jpaTransactionManager")
-  public void handleOrderCreatedEvent(@Payload OrderCreatedEvent orderCreatedEvent) {
+  public void reserveFoodAfterOrderCreatedEvent(@Payload OrderCreatedEvent orderCreatedEvent) {
     try {
       foodService.reserve(orderCreatedEvent.getFoodId(), orderCreatedEvent.getQuantity());
-    } catch(LockAcquisitionFailException e) {
+    } catch(Exception e) {
       log.error("오류가 발생하였습니다. message = {}", e.getMessage(), e);
-      sendOrderCreationFailEvent(orderCreatedEvent);
-      throw new NonRetryableException(e);
-    }
+      kafkaHandlerService.sendKafkaFailureEvent(
+          foodProcessFailEvent,
+          OrderCreationFailedEvent.toEvent(orderCreatedEvent)
+      );
 
-    try {
-      KafkaFoodHandler kafkaFoodHandler = new KafkaFoodHandler(kafkaTemplate);
-      kafkaFoodHandler.handleFoodReservationEvent(foodReservationEvent, orderCreatedEvent);
-    } catch (Exception e) {
-      log.error("오류가 발생하였습니다. message = {}", e.getMessage(), e);
-      sendOrderCreationFailEvent(orderCreatedEvent);
       throw e;
     }
+
+    sendKafkaEvent(orderCreatedEvent);
   }
 
-  @RequiredArgsConstructor
-  static class KafkaFoodHandler {
+  private void sendKafkaEvent(OrderCreatedEvent orderCreatedEvent) {
+    try {
+      kafkaHandlerService.sendKafkaEvent(foodReservationEvent, orderCreatedEvent);
+    } catch (Exception e) {
+      log.error("오류가 발생하였습니다. message = {}", e.getMessage(), e);
+      kafkaHandlerService.sendKafkaFailureEvent(
+          foodProcessFailEvent,
+          OrderCreationFailedEvent.toEvent(orderCreatedEvent)
+      );
 
-    private final KafkaTemplate<String, Object> kafkaTemplate;
-
-    @KafkaRetry
-    @Transactional(propagation = REQUIRES_NEW)
-    public void handleFoodReservationEvent(String foodReservationEvent, OrderCreatedEvent orderCreatedEvent) {
-      try {
-        kafkaTemplate.send(foodReservationEvent, orderCreatedEvent);
-      } catch(Exception e) {
-        log.error("오류가 발생하였습니다. message = {}", e.getMessage(), e);
-        throw new FoodApplicationKafkaRetryableException(e);
-      }
+      throw e;
     }
-  }
-
-  private void sendOrderCreationFailEvent(OrderCreatedEvent orderCreatedEvent) {
-    kafkaTemplate.send(foodProcessFailEvent, OrderCreationFailedEvent.toEvent(orderCreatedEvent));
   }
 }
