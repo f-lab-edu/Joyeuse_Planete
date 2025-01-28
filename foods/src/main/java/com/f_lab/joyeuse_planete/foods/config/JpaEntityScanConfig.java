aaf902289d527ea364c98119510e2443da15d685
koreanMike513 <paulryu1998@gmail.com>
2025-01-28 23:18:11 +0000

    fix: Fixed JpaEntity scan

--- foods/src/main/java/com/f_lab/joyeuse_planete/foods/config/JpaEntityScanConfig.java
+++ foods/src/main/java/com/f_lab/joyeuse_planete/foods/config/JpaEntityScanConfig.java
@@ -4,6 +4,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EntityScan(basePackages = { "com.f_lab.la_planete.core" })
+@EntityScan(basePackages = { "com.f_lab.joyeuse_planete.core" })
 public class JpaEntityScanConfig {
 }
