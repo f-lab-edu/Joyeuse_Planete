b69c9447fa2f4c7defe6aadf1fe72caa25feb2c6
koreanMike513 <paulryu1998@gmail.com>
2025-01-29 00:03:13 +0000

    refactor: Refactored entities and util classes

---
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/kafka/util/LogUtil.java
@@ -0,0 +1,15 @@
+package com.f_lab.joyeuse_planete.core.kafka.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogUtil {
+
+  public static void logException(Exception e) {
+
+  }
+
+  public static void logRetryException() {
+
+  }
+}
