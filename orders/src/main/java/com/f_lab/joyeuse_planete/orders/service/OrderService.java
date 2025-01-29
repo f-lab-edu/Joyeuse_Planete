d5ba45ba3438971a3480fedc21de1055cca40507
koreanMike513 <paulryu1998@gmail.com>
2025-01-29 00:25:34 +0000

    feat: Added OrderRepositoryCustom

--- orders/src/main/java/com/f_lab/joyeuse_planete/orders/service/OrderService.java
+++ orders/src/main/java/com/f_lab/joyeuse_planete/orders/service/OrderService.java
@@ -2,13 +2,18 @@ package com.f_lab.joyeuse_planete.orders.service;
 
 
 
+import com.f_lab.joyeuse_planete.core.domain.Order;
+import com.f_lab.joyeuse_planete.core.kafka.service.KafkaService;
+import com.f_lab.joyeuse_planete.orders.domain.OrderSearchCondition;
+import com.f_lab.joyeuse_planete.orders.dto.response.OrderDTO;
 import com.f_lab.joyeuse_planete.orders.dto.request.OrderCreateRequestDTO;
 import com.f_lab.joyeuse_planete.orders.dto.response.OrderCreateResponseDTO;
 import com.f_lab.joyeuse_planete.orders.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,43 +24,36 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
   private final OrderRepository orderRepository;
-
-  private final KafkaTemplate<String, Object> kafkaTemplate;
+  private final KafkaService kafkaService;
 
   @Value("${orders.events.topic.name}")
   String ORDER_CREATED_EVENT;
 
+  public Page<OrderDTO> findOrders(OrderSearchCondition condition, Pageable pageable) {
+    return orderRepository.findOrders(condition, pageable);
+  }
+
   @Transactional
   public OrderCreateResponseDTO createFoodOrder(OrderCreateRequestDTO request) {
-    log.info("request={}", request);
+    Order order = request.toEntity();
     try {
-      kafkaTemplate.send(ORDER_CREATED_EVENT, request.toEvent());
+      orderRepository.save(order);
     } catch (Exception e) {
-      throw new OrderCreatedFailureException(e);
+      log.error("오류가 발생하였습니다. message = {}", e.getMessage(), e);
+
     }
 
-    return new OrderCreateResponseDTO("CREATED");
+    sendKafkaOrderCreatedEvent(request, order);
+
+    return new OrderCreateResponseDTO("PROCESSING");
   }
 
 
-  static class OrderCreatedFailureException extends RuntimeException {
-    public OrderCreatedFailureException() {
-    }
-
-    public OrderCreatedFailureException(String message) {
-      super(message);
-    }
-
-    public OrderCreatedFailureException(String message, Throwable cause) {
-      super(message, cause);
-    }
-
-    public OrderCreatedFailureException(Throwable cause) {
-      super(cause);
-    }
-
-    public OrderCreatedFailureException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-      super(message, cause, enableSuppression, writableStackTrace);
+  public void sendKafkaOrderCreatedEvent(OrderCreateRequestDTO request, Order order) {
+    try {
+      kafkaService.sendKafkaEvent(ORDER_CREATED_EVENT, request.toEvent(order.getId()));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
   }
 }
