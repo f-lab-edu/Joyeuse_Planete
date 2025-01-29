0472b88de6dea0d4adee7e055cdda83055eef8fc
koreanMike513 <paulryu1998@gmail.com>
2025-01-29 16:55:51 +0000

    refactor: Refactored LogUtil class

--- core/src/main/java/com/f_lab/joyeuse_planete/core/kafka/aspect/KafkaRetryAspect.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/kafka/aspect/KafkaRetryAspect.java
@@ -2,6 +2,7 @@ package com.f_lab.joyeuse_planete.core.kafka.aspect;
 
 import com.f_lab.joyeuse_planete.core.exceptions.JoyeusePlaneteApplicationException;
 import com.f_lab.joyeuse_planete.core.exceptions.ErrorCode;
+import com.f_lab.joyeuse_planete.core.util.log.LogUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -20,18 +21,18 @@ public class KafkaRetryAspect {
 
   @Around("@annotation(com.f_lab.joyeuse_planete.core.kafka.aspect.KafkaRetry)")
   public Object kafkaRetry(ProceedingJoinPoint joinPoint) {
-    int attempt = 0;
+    int attempts = 0;
 
-    while(attempt < MAX_RETRY) {
+    while (attempts < MAX_RETRY) {
       try {
         return joinPoint.proceed();
       } catch (Throwable e) {
-        log.warn("시도 횟수={}, 메서드={}", attempt, joinPoint.getSignature());
-        attempt++;
+        LogUtil.retry(++attempts, joinPoint.getSignature().toString());
 
         try {
           Thread.sleep(STOP_INTERVAL);
         } catch (InterruptedException ex) {
+          LogUtil.exception(joinPoint.getSignature().toString(), ex);
           throw new JoyeusePlaneteApplicationException(ErrorCode.KAFKA_RETRY_FAIL_EXCEPTION, ex);
         }
       }
