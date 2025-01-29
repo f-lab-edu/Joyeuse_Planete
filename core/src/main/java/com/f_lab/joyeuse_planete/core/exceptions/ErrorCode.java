c24402089ae72f77388019801f10f45735b09d84
koreanMike513 <paulryu1998@gmail.com>
2025-01-29 22:42:03 +0000

    update: Updated Food entity and Errorcodes

--- core/src/main/java/com/f_lab/joyeuse_planete/core/exceptions/ErrorCode.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/exceptions/ErrorCode.java
@@ -10,6 +10,8 @@ import static lombok.AccessLevel.PRIVATE;
 public enum ErrorCode {
   FOOD_NOT_EXIST_EXCEPTION("상품이 존재하지 않습니다.", 400),
   FOOD_NOT_ENOUGH_STOCK("상품의 수량이 부족합니다", 409),
+  FOOD_QUANTITY_OVERFLOW("상품의 수량이 최대 값을 넘었습니다.", 406),
+  ORDER_NOT_EXIST_EXCEPTION("존재하지 않는 주문입니다.", 406),
 
   // LOCK
   LOCK_ACQUISITION_FAIL_EXCEPTION("현재 너무 많은 요청을 처리하고 있습니다. 다시 시도해주세요 (락)",503),
