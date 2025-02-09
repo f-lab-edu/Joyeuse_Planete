fde54fcdccf662169ea5958ebf0fbd465185c9f9
koreanMike513 <paulryu1998@gmail.com>
2025-02-09 18:26:17 +0000

    chore: Added @Timed to services

--- payment/src/main/java/com/f_lab/joyeuse_planete/payment/service/PaymentService.java
+++ payment/src/main/java/com/f_lab/joyeuse_planete/payment/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.f_lab.joyeuse_planete.payment.service;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@Timed("payment")
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
