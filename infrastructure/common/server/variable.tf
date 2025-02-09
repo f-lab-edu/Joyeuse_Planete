4a1c966f8574dc122040c9a0399c30b89cc2ad53
koreanMike513 <paulryu1998@gmail.com>
2025-02-09 15:40:41 +0000

    chore: Added terraform cd pipeline

---
+++ infrastructure/common/server/variable.tf
@@ -0,0 +1,31 @@
+variable "server_ami" {
+
+}
+
+variable "server_instance_type" {
+
+}
+
+variable "app_region" {
+
+}
+
+variable "docker_image" {
+
+}
+
+variable "server_count" {
+
+}
+
+variable "KAFKA_SERVER_IP" {
+
+}
+
+variable "aws_security_group_id" {
+  description = "Security group ID for EC2"
+}
+
+variable "aws_key_pair_name" {
+  description = "Key Pair Name for EC2"
+}
