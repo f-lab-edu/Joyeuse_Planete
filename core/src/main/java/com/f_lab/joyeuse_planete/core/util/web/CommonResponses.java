f353d531e1f81e868fde7a72878e051f44c2ae6d
koreanMike513 <paulryu1998@gmail.com>
2025-02-16 16:58:41 +0000

    feat: Added util and error translator

--- core/src/main/java/com/f_lab/joyeuse_planete/core/util/web/CommonResponses.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/util/web/CommonResponses.java
@@ -5,4 +5,5 @@ public abstract class CommonResponses {
   public static final String CREATE_SUCCESS = "성공적으로 생성되었습니다.";
   public static final String UPDATE_SUCCESS = "업데이트 요청이 완료되었습니다.";
   public static final String DELETE_SUCCESS = "삭제 요청이 완료되었습니다.";
+  public static final String PONG = "pong";
 }
