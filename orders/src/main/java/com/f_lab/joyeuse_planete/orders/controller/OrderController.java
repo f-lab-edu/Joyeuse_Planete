66434a5c044c56bc5093dd7d72a867bb9510329c
koreanMike513 <paulryu1998@gmail.com>
2025-02-16 21:53:32 +0000

    fix: Fixed compile error

--- orders/src/main/java/com/f_lab/joyeuse_planete/orders/controller/OrderController.java
+++ orders/src/main/java/com/f_lab/joyeuse_planete/orders/controller/OrderController.java
@@ -1,19 +1,32 @@
 package com.f_lab.joyeuse_planete.orders.controller;
 
 
+
+import com.f_lab.joyeuse_planete.core.util.web.CommonResponses;
 import com.f_lab.joyeuse_planete.orders.dto.request.OrderCreateRequestDTO;
 import com.f_lab.joyeuse_planete.orders.dto.request.OrderSearchCondition;
-import com.f_lab.joyeuse_planete.orders.dto.response.OrderCreateResponseDTO;
 import com.f_lab.joyeuse_planete.orders.dto.response.OrderDTO;
+import com.f_lab.joyeuse_planete.orders.dto.response.OrderPollingResponseDTO;
 import com.f_lab.joyeuse_planete.orders.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 
 
 @Slf4j
@@ -26,19 +39,47 @@ public class OrderController {
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public Page<OrderDTO> findOrders(@ModelAttribute OrderSearchCondition condition) {
+  public Page<OrderDTO> getOrderList(@ModelAttribute OrderSearchCondition condition) {
     PageRequest pageRequest = PageRequest.of(condition.getPage(), condition.getSize());
 
-    return orderService.findOrders(condition, pageRequest);
+    return orderService.getOrderList(condition, pageRequest);
+  }
+
+  @GetMapping("/{orderId}")
+  public ResponseEntity<OrderDTO> getOrder(@PathVariable("orderId") Long orderId) {
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(orderService.getOrder(orderId));
   }
 
   @PostMapping("/foods")
-  public ResponseEntity<OrderCreateResponseDTO> createFoodOrder(
-      @RequestBody OrderCreateRequestDTO orderCreateRequestDTO
-  ) {
+  public ResponseEntity<String> createFoodOrder(@RequestBody OrderCreateRequestDTO orderCreateRequestDTO) {
+    orderService.createFoodOrder(orderCreateRequestDTO);
 
     return ResponseEntity
         .status(HttpStatus.CREATED)
-        .body(orderService.createFoodOrder(orderCreateRequestDTO));
+        .body(CommonResponses.CREATE_SUCCESS);
+  }
+
+  @DeleteMapping("/member/{orderId}")
+  public ResponseEntity<String> deleteMemberOrder(@PathVariable("orderId") Long orderId) {
+    orderService.deleteOrderByMember(orderId);
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(CommonResponses.DELETE_SUCCESS);
+  }
+
+  @GetMapping("{orderId}/polling/order-status")
+  public ResponseEntity<OrderPollingResponseDTO> polling(@PathVariable Long orderId) {
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(orderService.polling(orderId));
+  }
+
+  @GetMapping("/ping")
+  public ResponseEntity<String> healthcheck() {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(CommonResponses.PONG);
   }
 }
