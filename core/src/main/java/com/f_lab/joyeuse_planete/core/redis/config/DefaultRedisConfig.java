a63795e3bf1baaabd83d652a89452ef2be1e14b2
koreanMike513 <paulryu1998@gmail.com>
2025-02-24 11:52:06 +0000

    fix: Fixed redis config

--- core/src/main/java/com/f_lab/joyeuse_planete/core/redis/config/DefaultRedisConfig.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/redis/config/DefaultRedisConfig.java
@@ -1,24 +1,31 @@
 package com.f_lab.joyeuse_planete.core.redis.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-public class RedisConfig {
+public abstract class DefaultRedisConfig {
 
   @Value("${spring.data.redis.host}")
   private String REDIS_HOST;
 
-  @Value("${spring.data.redis.PORT}")
+  @Value("${spring.data.redis.port}")
   private int REDIS_PORT;
 
   @Value("${spring.data.redis.password:none}")
   private String REDIS_PASSWORD;
 
+  @Profile("!prod")
+  @Bean
   public RedisConnectionFactory redisConnectionFactoryDefault() {
     return new LettuceConnectionFactory(REDIS_HOST, REDIS_PORT);
   }
+
+  @Profile("prod")
+  @Bean
   public RedisConnectionFactory redisConnectionFactory() {
     RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
     config.setPassword(REDIS_PASSWORD);
