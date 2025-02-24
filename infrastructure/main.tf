af9a750d125c3e4ad06825559af34a7da39a2298
koreanMike513 <paulryu1998@gmail.com>
2025-02-24 09:20:57 +0000

    refactor: Refactored terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,14 +11,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-data "aws_instance" "KAFKA_SERVER" {
-  instance_id = "i-00a35c8a2a342c55e"
-}
-
-data "aws_instance" "MONITORING_SERVER" {
-  instance_id = "i-059a48a5d68268fa8"
-}
-
 module "key_pair" {
   source = "./common/key"
 }
@@ -32,8 +24,7 @@ module "foods" {
   docker_food_image     = var.DOCKER_FOOD_IMAGE
   aws_security_group_id = module.securities.security_group_id
   aws_key_pair_name     = module.key_pair.aws_key_pair_name
-  KAFKA_SERVER_IP       = data.aws_instance.KAFKA_SERVER.public_ip
-  MONITORING_SERVER_IP  = data.aws_instance.MONITORING_SERVER.public_ip
+  KAFKA_SERVER_IP       = var.KAFKA_SERVER_IP
   DATABASE_URL          = var.DATABASE_URL
   DATABASE_USERNAME     = var.DATABASE_USERNAME
   DATABASE_PASSWORD     = var.DATABASE_PASSWORD
@@ -44,8 +35,7 @@ module "orders" {
   docker_orders_image   = var.DOCKER_ORDERS_IMAGE
   aws_security_group_id = module.securities.security_group_id
   aws_key_pair_name     = module.key_pair.aws_key_pair_name
-  KAFKA_SERVER_IP       = data.aws_instance.KAFKA_SERVER.public_ip
-  MONITORING_SERVER_IP  = data.aws_instance.MONITORING_SERVER.public_ip
+  KAFKA_SERVER_IP       = var.KAFKA_SERVER_IP
   DATABASE_URL          = var.DATABASE_URL
   DATABASE_USERNAME     = var.DATABASE_USERNAME
   DATABASE_PASSWORD     = var.DATABASE_PASSWORD
@@ -56,8 +46,7 @@ module "notifications" {
   docker_notifications_image = var.DOCKER_NOTIFICATIONS_IMAGE
   aws_security_group_id      = module.securities.security_group_id
   aws_key_pair_name          = module.key_pair.aws_key_pair_name
-  KAFKA_SERVER_IP            = data.aws_instance.KAFKA_SERVER.public_ip
-  MONITORING_SERVER_IP       = data.aws_instance.MONITORING_SERVER.public_ip
+  KAFKA_SERVER_IP            = var.KAFKA_SERVER_IP
   DATABASE_URL               = var.DATABASE_URL
   DATABASE_USERNAME          = var.DATABASE_USERNAME
   DATABASE_PASSWORD          = var.DATABASE_PASSWORD
@@ -68,8 +57,7 @@ module "payment" {
   docker_payment_image  = var.DOCKER_PAYMENT_IMAGE
   aws_security_group_id = module.securities.security_group_id
   aws_key_pair_name     = module.key_pair.aws_key_pair_name
-  KAFKA_SERVER_IP       = data.aws_instance.KAFKA_SERVER.public_ip
-  MONITORING_SERVER_IP  = data.aws_instance.MONITORING_SERVER.public_ip
+  KAFKA_SERVER_IP       = var.KAFKA_SERVER_IP
   DATABASE_URL          = var.DATABASE_URL
   DATABASE_USERNAME     = var.DATABASE_USERNAME
   DATABASE_PASSWORD     = var.DATABASE_PASSWORD
