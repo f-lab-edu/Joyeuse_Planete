bc6fafd4ade334c5432d6bbdf4b1415eb98a6ba6
koreanMike513 <paulryu1998@gmail.com>
2025-03-07 16:35:51 +0000

    feat: Added core spring aop tests

--- core/src/main/java/com/f_lab/joyeuse_planete/core/kafka/domain/DeadLetterStatus.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/kafka/domain/DeadLetterStatus.java
@@ -1,5 +1,5 @@
 package com.f_lab.joyeuse_planete.core.kafka.domain;
 
 public enum DeadLetterStatus {
-  PENDING, REQUEUED, FAILED_INVALID_FOR_REQUEUE, FAILED_AFTER_MAX_RETRIES,
+  PENDING, REQUEUED, INVALID_FOR_REQUEUE,
 }
