fc0ce8970ffa0bbb9ea16e701853a9829b1da529
koreanMike513 <paulryu1998@gmail.com>
2025-03-07 14:28:56 +0000

    update: Added spring aop to orders and payments project, updated jpa entity scan config, and removedunnecessary aop

--- payment/src/main/java/com/f_lab/joyeuse_planete/payment/config/JpaEntityScanConfig.java
+++ payment/src/main/java/com/f_lab/joyeuse_planete/payment/config/JpaEntityScanConfig.java
@@ -2,8 +2,15 @@ package com.f_lab.joyeuse_planete.payment.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(
+    basePackages = {
+        "com.f_lab.joyeuse_planete.payment.repository",
+        "com.f_lab.joyeuse_planete.core.kafka.repository"
+    }
+)
 @EntityScan(basePackages = { "com.f_lab.joyeuse_planete.core" })
 public class JpaEntityScanConfig {
 }
