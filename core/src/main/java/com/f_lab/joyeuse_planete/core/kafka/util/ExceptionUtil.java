51087b616839001977e7c262e189e61a347622c9
koreanMike513 <paulryu1998@gmail.com>
2025-03-07 13:48:55 +0000

    refactor: Refactored kafka deadletter annotation

--- core/src/main/java/com/f_lab/joyeuse_planete/core/kafka/util/ExceptionUtil.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/kafka/util/ExceptionUtil.java
@@ -1,19 +1,20 @@
 package com.f_lab.joyeuse_planete.core.kafka.util;
 
 import com.f_lab.joyeuse_planete.core.exceptions.ErrorCode;
-
 import org.springframework.kafka.listener.ListenerExecutionFailedException;
 
-import java.util.List;
+import java.util.Set;
 
 public class ExceptionUtil {
-  private static final List<Class<?>> unWrapList = List.of(
+  private static final Set<Class<?>> unWrapList = Set.of(
       ListenerExecutionFailedException.class
   );
 
-  private static final List<String> nonRequeueList = List.of(
+  private static final Set<String> nonRequeueList = Set.of(
       ErrorCode.FOOD_NOT_ENOUGH_STOCK.getDescription(),
-      ErrorCode.FOOD_NOT_EXIST_EXCEPTION.getDescription()
+      ErrorCode.FOOD_NOT_EXIST_EXCEPTION.getDescription(),
+      ErrorCode.UNKNOWN_EXCEPTION.getDescription(),
+      ErrorCode.KAFKA_RETRY_NOT_ACCEPTABLE_EXCEPTION.getDescription()
   );
 
   public static Exception unwrap(Exception e) {
