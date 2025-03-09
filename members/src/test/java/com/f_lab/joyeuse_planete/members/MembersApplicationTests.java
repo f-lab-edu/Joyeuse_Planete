4e24ef30935838ed6c7a11129858ad73f23dde70
koreanMike513 <paulryu1998@gmail.com>
2025-03-09 11:56:31 +0000

    feat: Added members project

---
+++ members/src/test/java/com/f_lab/joyeuse_planete/members/MembersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f_lab.joyeuse_planete.members;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
