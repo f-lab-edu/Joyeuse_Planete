0f5e414836f05287742bb64c9c85b7945d78e96d
koreanMike513 <paulryu1998@gmail.com>
2025-03-10 18:01:48 +0000

    feat: Added jwt

--- core/src/main/java/com/f_lab/joyeuse_planete/core/util/time/TimeConstants.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/util/time/TimeConstants.java
@@ -7,11 +7,14 @@ public class TimeConstants {
     public static final String FIVE_SECONDS = "5000";
     public static final String ONE_SECOND = "1000";
     public static final String THIRTY_MINUTES = "180000";
+    public static final String ONE_DAY = "86400000";
+
   }
 
   public static class TimeConstantsMillis {
     public static final int FIVE_SECONDS = 5000;
     public static final int ONE_SECOND = 1000;
     public static final int THIRTY_MINUTES = 180000;
+    public static final int ONE_DAY = 86400000;
   }
 }
