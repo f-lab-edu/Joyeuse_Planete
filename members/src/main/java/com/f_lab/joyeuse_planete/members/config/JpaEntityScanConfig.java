ada8f8ee09edb642ad16a026849f2bd4dd48991f
koreanMike513 <paulryu1998@gmail.com>
2025-03-12 21:33:32 +0000

    feat: Integrated spring security with JWT

--- members/src/main/java/com/f_lab/joyeuse_planete/members/config/JpaEntityScanConfig.java
+++ members/src/main/java/com/f_lab/joyeuse_planete/members/config/JpaEntityScanConfig.java
@@ -1,9 +1,19 @@
 package com.f_lab.joyeuse_planete.members.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 @Configuration
 @EntityScan(basePackages = { "com.f_lab.joyeuse_planete.core" })
 public class JpaEntityScanConfig {
+
+  @Primary
+  @Bean(name = "transactionManager")
+  public TransactionManager transactionManager() {
+    return new JpaTransactionManager();
+  }
 }
---
+++ members/src/main/java/com/f_lab/joyeuse_planete/members/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.f_lab.joyeuse_planete.members.config;
+
+import com.f_lab.joyeuse_planete.members.config.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final JwtFilter jwtFilter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    return http
+
+        .csrf(AbstractHttpConfigurer::disable)
+
+        .cors(withDefaults())
+
+        .formLogin(AbstractHttpConfigurer::disable)
+
+        .httpBasic(AbstractHttpConfigurer::disable)
+
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+//        .exceptionHandling()
+
+        .build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return username -> null;
+  }
+}
