61a92c2dec83ea7097c3b11d58fb0b7338797180
koreanMike513 <paulryu1998@gmail.com>
2025-03-12 21:33:32 +0000

    feat: Integrated spring security with JWT

--- members/src/main/java/com/f_lab/joyeuse_planete/members/controller/MemberController.java
+++ members/src/main/java/com/f_lab/joyeuse_planete/members/controller/MemberController.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,6 +32,7 @@ public class MemberController {
 
   private final MemberService memberService;
 
+  @PreAuthorize("#memberId == authentication.principal")
   @GetMapping("/{memberId}")
   public ResponseEntity<ResultResponse> getMember(@PathVariable("memberId") Long memberId) {
 
@@ -49,15 +52,27 @@ public class MemberController {
         .body(ResultResponse.of(CommonResponses.CREATE_SUCCESS, HttpStatus.CREATED.value()));
   }
 
-
   @PostMapping("/signin")
-  public ResponseEntity<ResultResponse> signin(@RequestBody @Valid SigninRequestDTO request) {
+  public ResponseEntity<ResultResponse> signin(
+      @RequestBody @Valid SigninRequestDTO request) {
 
     return ResponseEntity
         .status(HttpStatus.CREATED)
         .body(memberService.signin(request));
   }
 
+
+  @PreAuthorize("hasRole('MEMBER') || hasRole('ADMIN')")
+  @PostMapping("/signout/{memberId}")
+  public ResponseEntity<ResultResponse> signout(Authentication authentication) {
+
+    memberService.signout((Long) authentication.getPrincipal());
+
+    return ResponseEntity
+        .ok(ResultResponse.of(CommonResponses.DELETE_SUCCESS, HttpStatus.OK.value()));
+  }
+
+  @PreAuthorize("#memberId == authentication.principal")
   @PutMapping("/{memberId}")
   public ResponseEntity<ResultResponse> updateMember(
       @RequestBody @Valid MemberUpdateRequestDTO request,
@@ -70,6 +85,7 @@ public class MemberController {
         .body(ResultResponse.of(CommonResponses.UPDATE_SUCCESS, HttpStatus.OK.value()));
   }
 
+  @PreAuthorize("#memberId == authentication.principal")
   @DeleteMapping("/{memberId}")
   public ResponseEntity<ResultResponse> deleteMember(@PathVariable("memberId") Long memberId) {
 
