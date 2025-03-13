afd85ff53bb752a51dcb8a54179ee600e210a2e0
koreanMike513 <paulryu1998@gmail.com>
2025-03-13 19:40:47 +0000

    fix: Fixed tests and added exception handler

---
+++ members/src/main/java/com/f_lab/joyeuse_planete/members/config/security/filter/JwtExceptionFilter.java
@@ -0,0 +1,61 @@
+package com.f_lab.joyeuse_planete.members.config.security.filter;
+
+import com.f_lab.joyeuse_planete.core.exceptions.JoyeusePlaneteApplicationException;
+import com.f_lab.joyeuse_planete.core.util.log.LogUtil;
+import com.f_lab.joyeuse_planete.core.util.web.ResultResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    try {
+      filterChain.doFilter(request, response);
+    } catch (JoyeusePlaneteApplicationException e) {
+      sendApplicationErrorResponse(response, e);
+    } catch (Exception e) {
+      sendErrorResponse(response, e);
+    }
+  }
+
+  private void sendErrorResponse(
+      HttpServletResponse response, Exception e) throws IOException {
+
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+    response.getWriter().write(
+        new ObjectMapper().writeValueAsString(
+            ResultResponse.of(
+                e.getMessage(),
+                HttpStatus.UNAUTHORIZED.value())
+        )
+    );
+  }
+
+  private void sendApplicationErrorResponse(
+      HttpServletResponse response, JoyeusePlaneteApplicationException e) throws IOException {
+
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+    response.getWriter().write(
+        new ObjectMapper().writeValueAsString(
+            ResultResponse.of(
+                e.getErrorCode().getDescription(),
+                e.getErrorCode().getStatus())
+        )
+    );
+  }
+}
