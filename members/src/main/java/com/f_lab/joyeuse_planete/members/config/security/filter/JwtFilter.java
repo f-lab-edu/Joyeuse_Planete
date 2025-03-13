f608f8a1d3eead0c038cef4cb633ad0eb5a91703
koreanMike513 <paulryu1998@gmail.com>
2025-03-13 19:40:47 +0000

    fix: Fixed tests and added exception handler

--- members/src/main/java/com/f_lab/joyeuse_planete/members/config/security/filter/JwtFilter.java
+++ members/src/main/java/com/f_lab/joyeuse_planete/members/config/security/filter/JwtFilter.java
@@ -1,4 +1,4 @@
-package com.f_lab.joyeuse_planete.members.config.filter;
+package com.f_lab.joyeuse_planete.members.config.security.filter;
 
 import com.f_lab.joyeuse_planete.core.domain.MemberRole;
 import com.f_lab.joyeuse_planete.core.domain.RefreshToken;
@@ -37,7 +37,6 @@ public class JwtFilter extends OncePerRequestFilter {
   private static final String REFRESH_TOKEN_HEADER = "RefreshToken";
   private static final String GENERAL_TOKEN_PREFIX = "Bearer ";
 
-
 /**
  *  case1: access token 과 refresh token 모두가 만료된 경우 → 에러 발생 (재 로그인하여 둘다 새로 발급)
  *  case2: access token 은 만료됐지만, refresh token 은 유효한 경우 → refresh token 을 검증하여 access token 재발급
@@ -53,49 +52,16 @@ public class JwtFilter extends OncePerRequestFilter {
       if (StringUtils.hasText(accessToken) && StringUtils.hasText(refreshToken)) {
 
         // Access Token 과 Refresh Token 모두가 유효한 경우 → 정상 처리
-        if (!jwtUtil.hasExpired(accessToken) && !jwtUtil.hasExpired(refreshToken)) {
-          Long memberId = jwtUtil.getMemberId(accessToken);
-          MemberRole role = jwtUtil.getMemberRole(accessToken);
-
-          Authentication authentication = new UsernamePasswordAuthenticationToken(memberId, null, List.of(new SimpleGrantedAuthority(role.toString())));
-          saveAuthentication(authentication);
-        }
+        if (!jwtUtil.hasExpired(accessToken) && !jwtUtil.hasExpired(refreshToken))
+          processForValidToken(accessToken, refreshToken);
 
         // Access Token 이 만료되었지만 Refresh Token 은 유효한 경우 → Refresh Token 을 검증하여 Access Token 재발급
-        else if (jwtUtil.hasExpired(accessToken) && !jwtUtil.hasExpired(refreshToken)) {
-
-          // RefreshToken DB와 비교후 존재할 경우 AccessToken 발행
-          if (refreshTokenRepository.existsByToken(refreshToken)) {
-            Long memberId = jwtUtil.getMemberId(refreshToken);
-            MemberRole role = jwtUtil.getMemberRole(refreshToken);
-
-            String newAccessToken = jwtUtil.generateAccessToken(Payload.generate(memberId, role));
-            setAccessTokenToResponse(res, newAccessToken);
-
-            Authentication authentication = new UsernamePasswordAuthenticationToken(memberId, null, List.of(new SimpleGrantedAuthority(role.toString())));
-            saveAuthentication(authentication);
-          }
-
-          // 아닐 경우에 예외처리
-          else {
-            throw new JoyeusePlaneteApplicationException(ErrorCode.TOKEN_INVALID_EXCEPTION);
-          }
-        }
+        else if (jwtUtil.hasExpired(accessToken) && !jwtUtil.hasExpired(refreshToken))
+          processForExpiredAccessTokenAndInDateRefreshToken(res, refreshToken);
 
         // Access Token 이 유효하지만 Refresh Token 은 만료한 경우 → Refresh Token 재발급
-        else if (!jwtUtil.hasExpired(accessToken) && jwtUtil.hasExpired(refreshToken)) {
-          Long memberId = jwtUtil.getMemberId(accessToken);
-          MemberRole role = jwtUtil.getMemberRole(accessToken);
-
-          String newRefreshToken = jwtUtil.generateRefreshToken(Payload.generate(memberId, role));
-          setRefreshTokenToResponse(res, newRefreshToken);
-
-          refreshTokenRepository.delete(RefreshToken.from(refreshToken, memberId));
-          refreshTokenRepository.save(RefreshToken.from(newRefreshToken, memberId));
-
-          Authentication authentication = new UsernamePasswordAuthenticationToken(memberId, null, List.of(new SimpleGrantedAuthority(role.toString())));
-          saveAuthentication(authentication);
-        }
+        else if (!jwtUtil.hasExpired(accessToken) && jwtUtil.hasExpired(refreshToken))
+          processForInDateAccessTokenAndExpiredRefreshToken(res, accessToken, refreshToken);
 
         // Access Token 과 Refresh Token 모두가 만료된 경우 → 재로그인 요청
         else {
@@ -103,14 +69,60 @@ public class JwtFilter extends OncePerRequestFilter {
         }
       }
 
-    } catch(JoyeusePlaneteApplicationException e) {
+    } catch (JoyeusePlaneteApplicationException e) {
+      LogUtil.exception("JwtFilter.doFilterInternal (JoyeusePlaneteApplicationException)", e);
       throw e;
 
     } catch (Exception e) {
-      LogUtil.exception("JwtFilter.doFilterInternal", e);
+      LogUtil.exception("JwtFilter.doFilterInternal (Exception)", e);
+      throw new JoyeusePlaneteApplicationException(ErrorCode.UNKNOWN_EXCEPTION, e);
     }
 
     filterChain.doFilter(req, res);
+  }
+
+
+  // Access Token 과 Refresh Token 모두가 유효한 경우 → 정상 처리
+  private void processForValidToken(String accessToken, String refreshToken) {
+    Long memberId = jwtUtil.getMemberId(accessToken);
+    MemberRole role = jwtUtil.getMemberRole(accessToken);
+
+    Authentication authentication = new UsernamePasswordAuthenticationToken(memberId, null, List.of(new SimpleGrantedAuthority(role.toString())));
+    saveAuthentication(authentication);
+  }
+
+  private void processForExpiredAccessTokenAndInDateRefreshToken(HttpServletResponse res, String refreshToken) {
+    // RefreshToken DB와 비교후 존재할 경우 AccessToken 발행
+    if (refreshTokenRepository.existsByToken(refreshToken)) {
+      Long memberId = jwtUtil.getMemberId(refreshToken);
+      MemberRole role = jwtUtil.getMemberRole(refreshToken);
+
+      String newAccessToken = jwtUtil.generateAccessToken(Payload.generate(memberId, role));
+      setAccessTokenToResponse(res, newAccessToken);
+
+      Authentication authentication = new UsernamePasswordAuthenticationToken(memberId, null, List.of(new SimpleGrantedAuthority(role.toString())));
+      saveAuthentication(authentication);
+    }
+
+    // 아닐 경우에 예외처리
+    else {
+      throw new JoyeusePlaneteApplicationException(ErrorCode.TOKEN_INVALID_EXCEPTION);
+    }
+  }
+
+  // Access Token 이 유효하지만 Refresh Token 은 만료한 경우 → Refresh Token 재발급
+  private void processForInDateAccessTokenAndExpiredRefreshToken(HttpServletResponse res, String accessToken, String refreshToken) {
+    Long memberId = jwtUtil.getMemberId(accessToken);
+    MemberRole role = jwtUtil.getMemberRole(accessToken);
+
+    String newRefreshToken = jwtUtil.generateRefreshToken(Payload.generate(memberId, role));
+    setRefreshTokenToResponse(res, newRefreshToken);
+
+    refreshTokenRepository.delete(RefreshToken.from(refreshToken, memberId));
+    refreshTokenRepository.save(RefreshToken.from(newRefreshToken, memberId));
+
+    Authentication authentication = new UsernamePasswordAuthenticationToken(memberId, null, List.of(new SimpleGrantedAuthority(role.toString())));
+    saveAuthentication(authentication);
   }
 
 
