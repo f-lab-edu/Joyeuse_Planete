1ea3bd8d495f3fce837db6f23a967fe5fb211e74
koreanMike513 <paulryu1998@gmail.com>
2025-03-19 21:37:13 +0000

    feat: Applied set credentials to true for cookie

--- core/src/main/java/com/f_lab/joyeuse_planete/core/security/config/DefaultSecurityConfig.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/security/config/DefaultSecurityConfig.java
@@ -65,6 +65,7 @@ public abstract class DefaultSecurityConfig {
     corsConfiguration.addAllowedOrigin(DOMAIN);
     corsConfiguration.addAllowedHeader("*");
     corsConfiguration.addAllowedMethod("*");
+    corsConfiguration.setAllowCredentials(true);
     corsConfiguration.setMaxAge(ONE_HOUR);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
