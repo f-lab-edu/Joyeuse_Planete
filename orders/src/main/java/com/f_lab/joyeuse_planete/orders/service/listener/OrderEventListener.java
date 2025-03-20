f90039bfe836de81f0a2e73cb6313320dd17ebb0
koreanMike513 <paulryu1998@gmail.com>
2025-03-20 16:28:43 +0000

    feat: Applied async to Orders project
    - added async to event listeners
    - renamed AsyncConfig to DefaultAsyncConfig
    - added error status

--- orders/src/main/java/com/f_lab/joyeuse_planete/orders/service/listener/OrderEventListener.java
+++ orders/src/main/java/com/f_lab/joyeuse_planete/orders/service/listener/OrderEventListener.java
@@ -1,11 +1,15 @@
 package com.f_lab.joyeuse_planete.orders.service.listener;
 
 
+import com.f_lab.joyeuse_planete.core.domain.OrderStatus;
 import com.f_lab.joyeuse_planete.core.events.OrderCancelEvent;
 import com.f_lab.joyeuse_planete.core.events.OrderCreatedEvent;
 import com.f_lab.joyeuse_planete.core.kafka.service.KafkaService;
+import com.f_lab.joyeuse_planete.core.util.log.LogUtil;
+import com.f_lab.joyeuse_planete.orders.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -16,6 +20,7 @@ import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMI
 public class OrderEventListener {
 
   private final KafkaService kafkaService;
+  private final OrderService orderService;
 
   @Value("${orders.events.topics.create}")
   String ORDER_CREATED_EVENT;
@@ -23,13 +28,25 @@ public class OrderEventListener {
   @Value("${orders.events.topics.cancel}")
   String ORDER_CANCELLATION_EVENT;
 
+  @Async
   @TransactionalEventListener(phase = AFTER_COMMIT)
   public void on(OrderCreatedEvent event) {
-    kafkaService.sendKafkaEvent(ORDER_CREATED_EVENT, event);
+    try {
+      kafkaService.sendKafkaEvent(ORDER_CREATED_EVENT, event);
+    } catch (Exception e) {
+      LogUtil.exception("OrderEventListener.on (OrderCreatedEvent)", e);
+      orderService.updateOrderStatus(event.getOrderId(), OrderStatus.FAIL_ORDER);
+    }
   }
 
+  @Async
   @TransactionalEventListener(phase = AFTER_COMMIT)
   public void on(OrderCancelEvent event) {
-    kafkaService.sendKafkaEvent(ORDER_CANCELLATION_EVENT, event);
+    try {
+      kafkaService.sendKafkaEvent(ORDER_CANCELLATION_EVENT, event);
+    } catch (Exception e) {
+      LogUtil.exception("OrderEventListener.on (OrderCancelEvent)", e);
+      orderService.updateOrderStatus(event.getOrderId(), OrderStatus.FAIL_CANCEL);
+    }
   }
 }
