b17f0a2169a094f3d48e7eb42d7dc0d1c3255609
koreanMike513 <paulryu1998@gmail.com>
2025-03-20 16:28:43 +0000

    feat: Applied async to Orders project
    - added async to event listeners
    - renamed AsyncConfig to DefaultAsyncConfig
    - added error status

--- core/src/main/java/com/f_lab/joyeuse_planete/core/exceptions/ErrorCodeOrderStatusTranslator.java
+++ core/src/main/java/com/f_lab/joyeuse_planete/core/exceptions/ErrorCodeOrderStatusTranslator.java
@@ -8,11 +8,11 @@ public class ErrorCodeOrderStatusTranslator {
 
   private static final Map<ErrorCode, OrderStatus> ERROR_CODE_MAP = Map.of(
       ErrorCode.FOOD_NOT_ENOUGH_STOCK, OrderStatus.FAIL_FOOD_QUANTITY,
-      ErrorCode.FOOD_NOT_EXIST_EXCEPTION, OrderStatus.FAIL,
-      ErrorCode.ORDER_NOT_EXIST_EXCEPTION, OrderStatus.FAIL
+      ErrorCode.FOOD_NOT_EXIST_EXCEPTION, OrderStatus.FAIL_ORDER,
+      ErrorCode.ORDER_NOT_EXIST_EXCEPTION, OrderStatus.FAIL_ORDER
   );
 
   public static OrderStatus translate(ErrorCode errorCode) {
-    return ERROR_CODE_MAP.getOrDefault(errorCode, OrderStatus.FAIL);
+    return ERROR_CODE_MAP.getOrDefault(errorCode, OrderStatus.FAIL_ORDER);
   }
 }
